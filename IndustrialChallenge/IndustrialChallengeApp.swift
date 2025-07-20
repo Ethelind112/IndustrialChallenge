82458b64f2efbe8e0d78bab01b087fc81e20a2cc
Ethelind <Ethelindsm@gmail.com>
2025-07-20 23:25:40 +0700

    Initial Commit

---
+++ IndustrialChallenge/IndustrialChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IndustrialChallengeApp.swift
+//  IndustrialChallenge
+//
+//  Created by Ethelind Septiani Metta on 20/07/25.
+//
+
+import SwiftUI
+
+@main
+struct IndustrialChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
