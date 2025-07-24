5b8764ff7b4508ad3047b90d8c04390feba0697e
Ethelind <Ethelindsm@gmail.com>
2025-07-24 16:51:06 +0700

    [Feat] BorrowingNeedSheet + ExpenseSheet: Error Handling

--- IndustrialChallenge/Features/SiPlin/View/BorrowingNeedSheet.swift
+++ IndustrialChallenge/Features/SiPlin/View/BorrowingNeedSheet.swift
@@ -11,62 +11,84 @@ struct BorrowingNeedSheet: View {
     
     @Binding var borrowed: String
     @Binding var currSiPlinStep: SiPlinStep
+    @State var showToast = false
     
     var body: some View {
         NavigationStack {
-            VStack {
-                HeaaderBorrowingNeed()
-                
-                VStack {
-                    Text("Langsung dapatkan jumlah pinjaman yang sesuai dengan kapabilitasmu!")
-                        .font(.caption)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .multilineTextAlignment(.leading)
-                    
-                    Spacer()
-                    
-                    KetahuiSiPlin()
-                    
-                    Spacer()
-                    
-                    IsiPinjamComponent(borrowed: $borrowed)
-                    
-                    Spacer()
+            ZStack (alignment: .top) {
+                if showToast {
+                    ToastView(type: .warning, message: "Maksimum jumlah pinjaman adalah Rp60.000.000")
+                        .transition(.move(edge: .top).combined(with: .opacity))
+                        .zIndex(999)
+                        .onAppear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                                withAnimation {
+                                    showToast = false
+                                }
+                            }
+                        }
                 }
-                .padding(.horizontal, 20)
-                
-                Rectangle()
-                    .frame(height: 1, alignment: .bottom)
-                    .foregroundColor(Color("AdditionalColorLightGray"))
-                
-                Spacer()
                 
                 VStack {
+                    HeaaderBorrowingNeed()
+                    
+                    VStack {
+                        Text("Langsung dapatkan jumlah pinjaman yang sesuai dengan kapabilitasmu!")
+                            .font(.caption)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .multilineTextAlignment(.leading)
+                        
+                        Spacer()
+                        
+                        KetahuiSiPlin()
+                        
+                        Spacer()
+                        
+                        IsiPinjamComponent(borrowed: $borrowed)
+                        
+                        Spacer()
+                    }
+                    .padding(.horizontal, 20)
+                    
+                    Rectangle()
+                        .frame(height: 1, alignment: .bottom)
+                        .foregroundColor(Color("AdditionalColorLightGray"))
+                    
+                    Spacer()
                     
                     VStack {
                         
-                        NumberKeyboard(inputs: $borrowed)
-                            .padding(.horizontal, 30)
-                        
-                        Button {
-                            currSiPlinStep = .siPlinExpense
-                        } label: {
-                            LanjutButton()
-                                .padding(.top, 30)
+                        VStack {
+                            
+                            NumberKeyboard(inputs: $borrowed)
+                                .padding(.horizontal, 30)
+                            
+                            Button {
+                                currSiPlinStep = .siPlinExpense
+                            } label: {
+                                LanjutButton()
+                                    .padding(.top, 30)
+                            }
+    //                        .padding(.top)
+                            
                         }
-//                        .padding(.top)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                        .frame(height: 300)
                         
                     }
-                    .font(.title3)
-                    .fontWeight(.bold)
-                    .foregroundColor(.black)
-                    .frame(height: 300)
+                    .padding(.vertical, 40)
+                    
+                    Spacer()
                     
                 }
-                .padding(.vertical, 40)
-                
-                Spacer()
-                
+            }
+            .onChange(of: borrowed) {
+                if Int(borrowed.formatWithoutDot()) ?? 0 > maximumLimitPinjamanInt {
+                    showToast = true
+                    borrowed = maximumLimitPinjaman.formatAsDecimal()
+                }
             }
         }
     }
--- IndustrialChallenge/Features/SiPlin/View/ExpenseSheet.swift
+++ IndustrialChallenge/Features/SiPlin/View/ExpenseSheet.swift
@@ -12,57 +12,82 @@ struct ExpenseSheet: View {
     @ObservedObject var viewModel: SiPlinController
     @Binding var currSiPlinStep: SiPlinStep
     @State var computation: String = ""
+    @State var showToast = false
     
     var body: some View {
         NavigationView {
-            VStack {
-                VStack {
-                    
-                    HeaderExpense(currSiPlinStep: $currSiPlinStep, title: "Isi Pengeluaranmu")
-                    
-                    Spacer()
-                    
-                    Text("Dengan tahu penghasilan dan pengeluaranmu, SiPlin bisa rekomendasiin jumlah pinjaman yang paling pas buat kamu.")
-                        .font(.caption)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .multilineTextAlignment(.leading)
-                        
-                    Spacer()
-                    
-                    DetailExpense(viewModel: viewModel)
-                    
-                    Spacer()
-                    
-                    IsiPengeluaran(viewModel: viewModel, computation: $computation)
+            
+            ZStack (alignment: .top) {
+                
+                if showToast {
+                    ToastView(type: .warning, message: "Pengeluaranmu tidak bisa minus")
+                        .transition(.move(edge: .top).combined(with: .opacity))
+                        .zIndex(999)
+                        .onAppear {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                                withAnimation {
+                                    showToast = false
+                                }
+                            }
+                        }
                 }
-                .padding([.top, .horizontal], 20)
-                
-                Rectangle()
-                    .frame(height: 1, alignment: .bottom)
-                    .foregroundColor(Color("AdditionalColorLightGray"))
                 
                 VStack {
+                    VStack {
+                        
+                        HeaderExpense(currSiPlinStep: $currSiPlinStep, title: "Isi Pengeluaranmu")
+                        
+                        Spacer()
+                        
+                        Text("Dengan tahu penghasilan dan pengeluaranmu, SiPlin bisa rekomendasiin jumlah pinjaman yang paling pas buat kamu.")
+                            .font(.caption)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .multilineTextAlignment(.leading)
+                            
+                        Spacer()
+                        
+                        DetailExpense(viewModel: viewModel)
+                        
+                        Spacer()
+                        
+                        IsiPengeluaran(viewModel: viewModel, computation: $computation)
+                    }
+                    .padding([.top, .horizontal], 20)
+                    
+                    Rectangle()
+                        .frame(height: 1, alignment: .bottom)
+                        .foregroundColor(Color("AdditionalColorLightGray"))
                     
                     VStack {
                         
-                        Calculator(inputs: $viewModel.loanRequest.expense, computation: $computation)
-                            .padding(.horizontal, 30)
-                        
-                        Button {
-                            currSiPlinStep = .siPlinRecommendation
-                        } label: {
-                            LanjutButton()
-                                .padding(.top, 30)
+                        VStack {
+                            
+                            Calculator(inputs: $viewModel.loanRequest.expense, computation: $computation)
+                                .padding(.horizontal, 30)
+                            
+                            Button {
+                                currSiPlinStep = .siPlinRecommendation
+                            } label: {
+                                LanjutButton()
+                                    .padding(.top, 30)
+                            }
+    //                        .padding(.top, 1)
                         }
-//                        .padding(.top, 1)
+                        .font(.title3)
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                        .frame(height: 300)
                     }
-                    .font(.title3)
-                    .fontWeight(.bold)
-                    .foregroundColor(.black)
-                    .frame(height: 300)
+                    .padding(.vertical, 40)
+                    
                 }
-                .padding(.vertical, 40)
-                
+                .onChange(of: viewModel.expense) {
+                    if Int(viewModel.expense.formatWithoutDot()) ?? 0 < 0 {
+                        showToast = true
+                        viewModel.loanRequest.expense = "0"
+                        computation = ""
+                    }
+                }
             }
         }
     }
